e317fd11a0dcce8b72278152db4c01c2fc56447d
Korshak Sergey <61161255+SergeyK92@users.noreply.github.com>
2020-02-19 20:52:25 +0300

    создали класс копилочки)

---
+++ Money_Box.py
@@ -0,0 +1,22 @@
+class MoneyBox:
+    def __init__(self, capacity):
+        # конструктор с аргументом – вместимость копилки
+        self.count1 = 0
+        self.capacity = capacity
+    def can_add(self, v):
+    # True, если можно добавить v монет, False иначе
+        if self.capacity - self.count1 >= v:
+            return True
+        else:
+            return False
+    def add(self, v):
+    # положить v монет в копилку
+        if MoneyBox.can_add(self, v):
+            self.count1 += v
+
+x=MoneyBox(100) # создаем объект класса monebox
+x.add(15) # к объекту класса х применили метод add класса manebox
+print(x.count1)
+x.add(100)
+print(x.count1)
+x.can_add(15)
